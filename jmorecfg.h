89ff1d28ab1e8885b61b2752ce44f904708f2bd0
benb365 <liuyongchang1@huawei.com>
2022-08-25 12:26:06 +0000

    Fix redefine of INT32
    Signed-off-by: benb365 <liuyongchang1@huawei.com>

--- jmorecfg.h
+++ jmorecfg.h
@@ -172,11 +172,13 @@ typedef short INT16;
  * same regardless of any external headers that may be included.
  */
 
+#ifndef _LOS_COMPILER_H         /* LiteOS-M defines INT32 in los_compiler.h */
 #ifndef XMD_H                   /* X11/xmd.h correctly defines INT32 */
 #ifndef _BASETSD_H_             /* Microsoft defines it in basetsd.h */
 #ifndef _BASETSD_H              /* MinGW is slightly different */
 #ifndef QGLOBAL_H               /* Qt defines it in qglobal.h */
 typedef long INT32;
+#endif
 #endif
 #endif
 #endif
