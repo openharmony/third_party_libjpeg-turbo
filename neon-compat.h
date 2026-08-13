7110547c08621f494cbf3df49db80c317843cf63
zxy680pla <zhongxiangyu1@h-partners.com>
2026-08-13 13:46:56 +0800

    蓝黄差异同步
    Signed-off-by: zxy680pla <zhongxiangyu1@h-partners.com>

--- neon-compat.h
+++ neon-compat.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2020, D. R. Commander.  All Rights Reserved.
- * Copyright (C) 2020-2021, Arm Limited.  All Rights Reserved.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  *
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the authors be held liable for any damages
